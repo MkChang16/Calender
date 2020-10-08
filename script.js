ca5bbc38972603ae692d860819b2d43e35722443
MkChang16 <71153650+MkChang16@users.noreply.github.com>
2020-10-08 15:08:59 -0500

    Time is synced with classes. Working on local storage.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const currentDay = document.getElementById('dateTime')
-const momentDateTime = moment().format('LLLL');
+$(document).ready(function(){
 
-currentDay.innerText = "JSON.stringify(momentDateTime)"
-console.log(moment().format('LLLL'))
+const momentDateTime = moment().format('LLLL');
+$("#currentDay").text(momentDateTime)
+
+console.log(momentDateTime.toString())
+
+const currentTime = moment().hours();
+
+function updateColors(){
+$(".timeBlocks").each(function(){
+        var currentTimeBlock = parseInt($(this).children(".timeBlockTextArea").attr("id"))
+        if (currentTimeBlock === currentTime){
+            $(this).children(".timeBlockTextArea").addClass("present")
+        }
+
+        console.log(currentTimeBlock)
+
+        if (currentTimeBlock > currentTime){
+            $(this).children(".timeBlockTextArea").addClass("past")
+        }
+
+        if (currentTimeBlock < currentTime){
+            $(this).children(".timeBlockTextArea").addClass("future")
+        }
+
+        // current < currentTime
+})
+
+}
+updateColors()
+})
